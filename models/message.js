3f17f9f28f7e8fbcb128a2a20b2455f4cfc74bdf
Pramod Nair <pramod.nair@open.edu.au>
2016-08-22 12:27:46 +1000

    basic setup

---
+++ models/message.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+    content: { type: String, required: true },
+    user: { type: Schema.types.ObjectId, ref: 'User' }
+});
+
+module.exports = mongoose.model('Message', schema);
